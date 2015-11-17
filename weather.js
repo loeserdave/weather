9e98a117f59b3b94df2ff9b2eafe22a493276ded
loeserdave <d-loeser@kellogg.northwestern.edu>
2015-11-17 13:20:19 -0600

    updated

--- weather.js
+++ weather.js
@@ -7,34 +7,34 @@ $(function() {
     var currentTemp = data.currently.temperature;
     var currentSummary = data.currently.summary;
     var dailySummary = data.hourly.summary;
-    // var currentIcon = data.currently.icon
+    var currentIcon = data.currently.icon
     // test if statement
-    var currentIcon = "fog";
+    // var currentIcon = "test";
      if (currentIcon = "rain") {
-        var currentImage = '<img src="SVG/Cloud-Rain.svg">'
+        var currentImage = '<img src="SVG/Cloud-Rain.svg">';
       } else if (currentIcon = "clear-day") {
-        var currentImage = '<img src="SVG/Sun.svg">'
+        var currentImage = '<img src="SVG/Sun.svg">';
       } else if (currentIcon = "clear-night") {
-        var currentImage = '<img src="SVG/Moon.svg">'
+        var currentImage = '<img src="SVG/Moon.svg">';
       } else if (currentIcon = "snow") {
-        var currentImage = '<img src="SVG/Snowflakes.svg">'
+        var currentImage = '<img src="SVG/Snowflakes.svg">';
       } else if (currentIcon = "sleet") {
-        var currentImage = '<img src="SVG/Cloud-Snow.svg">'
+        var currentImage = '<img src="SVG/Cloud-Snow.svg">';
       } else if (currentIcon = "wind") {
-        var currentImage = '<img src="SVG/Wind.svg">'
+        var currentImage = '<img src="SVG/Wind.svg">';
       } else if (currentIcon = "fog") {
-        var currentImage = '<img src="SVG/Cloud-Fog.svg">'
+        var currentImage = '<img src="SVG/Cloud-Fog.svg">';
       } else if (currentIcon = "cloudy") {
-        var currentImage = '<img src="SVG/Cloud.svg">'
+        var currentImage = '<img src="SVG/Cloud.svg">';
       } else if (currentIcon = "partly-cloudy-day") {
-        var currentImage = '<img src="SVG/Cloud-Sun.svg">'
+        var currentImage = '<img src="SVG/Cloud-Sun.svg">';
       } else if (currentIcon = "partly-cloudy-night") {
-        var currentImage = '<img src="SVG/Cloud-Moon.svg">'
+        var currentImage = '<img src="SVG/Cloud-Moon.svg">';
       } else {
         var currentImage = "Unknown"
       }
 
-    var markup = '<div class="row"><div class="col-xs-4"><h3>Current Weather Near You:</h3><table class="table-bordered table-hover"><tbody><tr><td class="table-body-bold">Temperature</td><td>' + currentTemp + '<img id="fahrenheit" src="SVG/Degrees-Fahrenheit.svg"></tr><tr><td class="table-body-bold">Current Summary</td><td>' + currentSummary + currentImage + '</td></tr><tr><td class="table-body-bold">Daily Summary</td><td>' + dailySummary + '</tbody></div></div>'
+    var markup = '<div class="row"><div class="col-xs-4"><h3>Current Weather Near You:</h3><table class="table-bordered table-hover"><tbody><tr><td class="table-body-bold">Temperature</td><td>' + currentTemp + '<img id="fahrenheit" src="SVG/Degrees-Fahrenheit.svg"></tr><tr><td class="table-body-bold">Current Summary</td><td>' + currentSummary + currentImage + currentIcon + '</td></tr><tr><td class="table-body-bold">Daily Summary</td><td>' + dailySummary + '</tbody></div></div>'
     // End of your code
 
     $('.weather-report').html(markup);
